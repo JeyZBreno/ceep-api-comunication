1858b5bcd8e90e0236077d57729c5e8760c420a2
JeyZBreno <breno.m.aragao@gmail.com>
2023-10-16 22:44:53 -0300

    feat: adjusted code to get info from api

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.kt
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotasActivity.kt
@@ -2,17 +2,28 @@ package br.com.alura.ceep.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import br.com.alura.ceep.data.NotaApiImpl
+import br.com.alura.ceep.data.builder.RetrofitBuilder
+import br.com.alura.ceep.data.model.NotaResponse
+import br.com.alura.ceep.data.repository.NotaRepository
 import br.com.alura.ceep.database.AppDatabase
 import br.com.alura.ceep.databinding.ActivityListaNotasBinding
 import br.com.alura.ceep.extensions.vaiPara
+import br.com.alura.ceep.model.Nota
 import br.com.alura.ceep.ui.recyclerview.adapter.ListaNotasAdapter
+import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.http.GET
 
 class ListaNotasActivity : AppCompatActivity() {
 
@@ -22,8 +33,12 @@ class ListaNotasActivity : AppCompatActivity() {
     private val adapter by lazy {
         ListaNotasAdapter(this)
     }
-    private val dao by lazy {
-        AppDatabase.instancia(this).notaDao()
+
+    private val repository by lazy {
+        NotaRepository(
+            AppDatabase.instancia(this).notaDao(),
+            NotaApiImpl(),
+        )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,10 +47,55 @@ class ListaNotasActivity : AppCompatActivity() {
         configuraFab()
         configuraRecyclerView()
         lifecycleScope.launch {
+            launch {
+                atualizaTodas()
+            }
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 buscaNotas()
             }
         }
+    }
+
+    private suspend fun atualizaTodas() {
+        repository.atualizaTodas()
+    }
+
+    private fun retrofitSemCoroutines() {
+
+        //          Api Service
+        //        @GET("notas")
+        //        fun buscaTodas(): Call<List<NotaResponse>>
+        //
+        //
+        //          Implementação do Service
+        //        val call: Call<List<NotaResponse>> = RetrofitBuilder().notaService.buscaTodas()
+        //        lifecycleScope.launch(IO) {
+        //
+        //            val response: Response<List<NotaResponse>> = call.execute()
+        //            response.body()?.let { notasResponse ->
+        //                val notas: List<Nota> = notasResponse.map {
+        //                    it.nota
+        //                }
+        //                Log.i("ListaNotas", "onCreate: $notas")
+        //            }
+        //        }
+        //        call.enqueue(object : Callback<List<NotaResponse>?> {
+        //            override fun onResponse(
+        //                call: Call<List<NotaResponse>?>,
+        //                response: Response<List<NotaResponse>?>
+        //            ) {
+        //                response.body()?.let { notasResponse ->
+        //                    val notas: List<Nota> = notasResponse.map {
+        //                        it.nota
+        //                    }
+        //                    Log.i("ListaNotas", "onCreate: $notas")
+        //                }
+        //            }
+        //
+        //            override fun onFailure(call: Call<List<NotaResponse>?>, t: Throwable) {
+        //                Log.e("ListaNotas", "onFaliure: ", t)
+        //            }
+        //        })
     }
 
     private fun configuraFab() {
@@ -56,7 +116,7 @@ class ListaNotasActivity : AppCompatActivity() {
     }
 
     private suspend fun buscaNotas() {
-        dao.buscaTodas()
+        repository.buscaTodas()
             .collect { notasEncontradas ->
                 binding.activityListaNotasMensagemSemNotas.visibility =
                     if (notasEncontradas.isEmpty()) {
